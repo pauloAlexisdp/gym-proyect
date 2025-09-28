22f6a9e05cf6e4cb204a47a8cf89057152c9aae0
Paulo Alexis Parraguez Diaz <44139011+pauloAlexisdp@users.noreply.github.com>
2025-09-28 12:20:11 -0300

    Add initial config and folder structure

---
+++ backend/src/config/env.ts
@@ -0,0 +1,8 @@
+const config = {
+    env: process.env.NODE_ENV || "development",
+    port: Number(process.env.PORT || 3000),
+    jwtSecret: process.env.JWT_SECRET || "D3v3lopm3nT_s3cr3t",
+    databaseUrl: process.env.DATABASE_URL || ""
+};
+
+export default config;
